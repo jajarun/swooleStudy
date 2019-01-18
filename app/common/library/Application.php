c74e0fc008efbb03d02472beb2a241c1447f548f
majiahao@yiwei.com <hao136877>
2019-01-18 11:18:11 +0800

    update

---
+++ app/common/library/Application.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 19/1/2
+ * Time: 上午10:42
+ */
+
+namespace Common\library;
+
+
+class Application
+{
+
+    public function __construct()
+    {
+    }
+
+    public function run(){
+
+    }
+
+}
---
+++ app/common/library/Di.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 19/1/2
+ * Time: 上午10:42
+ */
+
+namespace Common\library;
+
+
+class Di
+{
+
+    private $contains = [];
+
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function set(){
+
+    }
+
+}
